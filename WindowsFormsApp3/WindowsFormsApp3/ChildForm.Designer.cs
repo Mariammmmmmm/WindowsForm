ffea5ea50a3f608d04a7be305a7e1a12f5cd4330
Mariammmmmmm <47477480+Mariammmmmmm@users.noreply.github.com>
2021-03-22 17:26:53 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/ChildForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class ChildForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChildTextBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.FormatMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToggleMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ChildTextBox
+            // 
+            this.ChildTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChildTextBox.Location = new System.Drawing.Point(0, 28);
+            this.ChildTextBox.Name = "ChildTextBox";
+            this.ChildTextBox.Size = new System.Drawing.Size(800, 422);
+            this.ChildTextBox.TabIndex = 0;
+            this.ChildTextBox.Text = "";
+            this.ChildTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FormatMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FormatMenuItem
+            // 
+            this.FormatMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToggleMenuItem});
+            this.FormatMenuItem.MergeAction = System.Windows.Forms.MergeAction.Insert;
+            this.FormatMenuItem.Name = "FormatMenuItem";
+            this.FormatMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.FormatMenuItem.Text = "F&ormat";
+            // 
+            // ToggleMenuItem
+            // 
+            this.ToggleMenuItem.Name = "ToggleMenuItem";
+            this.ToggleMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ToggleMenuItem.Text = "&Toggle Foreground";
+            this.ToggleMenuItem.Click += new System.EventHandler(this.ToggleMenuItem_Click);
+            // 
+            // ChildForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ChildTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ChildForm";
+            this.Text = "Child Form";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ChildTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem FormatMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ToggleMenuItem;
+    }
+}
---
+++ WindowsFormsApp3/WindowsFormsApp3/ParentForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments;
+
+        public ParentForm()
+        {
+            InitializeComponent();
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowsMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+            spWin.Text = "Windows is cascade";
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+            spWin.Text = "Windows is horizontal";
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+
+                    break;          
+
+                   case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+        }
+    }
+}
