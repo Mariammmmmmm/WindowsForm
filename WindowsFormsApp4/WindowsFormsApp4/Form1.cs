d118c3c132e2553953fdc65c8b14871990ca75af
Mariammmmmmm <47477480+Mariammmmmmm@users.noreply.github.com>
2021-03-22 17:28:09 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Drawing.Drawing2D.GraphicsPath myPath =
+                new System.Drawing.Drawing2D.GraphicsPath();
+
+            //myPath.AddEllipse(0, 0, this.Width, this.Height);
+
+            myPath.AddEllipse(0,0, this.Width, this.Height);
+            Region myRegion = new Region(myPath);
+            this.Region = myRegion;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 myF2 = new Form1();
+            myF2.StartPosition = FormStartPosition.Manual;
+            myF2.Location = new Point(this.Location.X + this.Width, this.Location.Y);
+            myF2.Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
